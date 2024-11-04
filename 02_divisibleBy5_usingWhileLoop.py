d5fc70ccfaf9f6a07cfe05af93d6e7a9b4617eae
nikita <nikitamathe12@gmail.com>
2024-11-04 20:46:19 +0530

    updated

--- 02_divisibleBy5_usingWhileLoop.py
+++ 02_divisibleBy5_usingWhileLoop.py
@@ -5,6 +5,7 @@ while number > 6 and number < 16:
         break
     elif number % 5 !=0:
         print(number,"is not divisible by 5")
+        number = int(input("Enter a new number between 6 and 16: ")) 
         
 else:
     print(number,"is out of range.")
